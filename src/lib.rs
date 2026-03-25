85da35851e945f1923a2d52d3fbc2f9ee8a23ae2
Xqhare <xqhare@web.de>
2026-03-26 00:21:06 +0100

    feat: Add XffValue, rework lib to expose XffValue and HpFloat

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,5 @@
-/// A drop-in replacement alias for high-precision f64
-#[allow(non_camel_case_types)]
-pub type f64 = Aequa;
+mod hp_float;
+pub use hp_float::*;
 
-/// A drop-in replacement alias for high-precision f32
-#[allow(non_camel_case_types)]
-pub type f32 = Aequa;
-
-mod impls;
-pub use impls::*;
-mod constants;
-pub use constants::*;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum AequaError {
-    InvalidScale,
-    InvalidValue,
-}
+mod value;
+pub use value::*;
