d40266e67676470dc13a7ba5e3b97af817e2b7f2
Xqhare <xqhare@web.de>
2026-06-28 01:32:51 +0200

    feat(LocalDateTime/from_rfc3339): now supports the full range of possible string formats as described in TOML 1.1.0

--- src/value/local_datetime.rs
+++ src/value/local_datetime.rs
@@ -22,3 +22,64 @@ impl std::fmt::Display for LocalDateTime {
         write!(f, "{}T{}", self.date, self.time)
     }
 }
+
+impl TryFrom<String> for LocalDateTime {
+    type Error = ();
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        value.as_str().try_into()
+    }
+}
+
+impl TryFrom<&str> for LocalDateTime {
+    type Error = ();
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if !value.contains('T') && !value.contains('t') && !value.contains(' ') || value.len() < 16
+        {
+            return Err(());
+        }
+        let split_pattern = {
+            if value.contains('t') {
+                't'
+            } else if value.contains('T') {
+                'T'
+            } else if value.contains(' ') {
+                ' '
+            } else {
+                return Err(());
+            }
+        };
+        let mut it = value.split(split_pattern);
+        let date = it.next().ok_or(())?.try_into().map_err(|_| ())?;
+        let time = it.next().ok_or(())?.try_into().map_err(|_| ())?;
+        Ok(Self::new(date, time))
+    }
+}
+
+#[test]
+fn parse_rfc3339() {
+    let str0 = "2023-09-03T02:10:00.987";
+    let l_dt0 = LocalDateTime::try_from(str0).unwrap();
+    assert_eq!(l_dt0.to_string(), str0);
+
+    let str1 = "1979-05-27T07:32:00";
+    let l_dt1 = LocalDateTime::try_from(str1).unwrap();
+    assert_eq!(l_dt1.to_string(), "1979-05-27T07:32:00.0");
+
+    let str2 = "1979-05-27T07:32:00.5";
+    let l_dt2 = LocalDateTime::try_from(str2).unwrap();
+    assert_eq!(l_dt2.to_string(), str2);
+
+    let str3 = "1979-05-27T07:32";
+    let l_dt3 = LocalDateTime::try_from(str3).unwrap();
+    assert_eq!(l_dt3.to_string(), "1979-05-27T07:32:00.0");
+
+    let str4 = "1979-05-27t07:32:00.5";
+    let l_dt4 = LocalDateTime::try_from(str4).unwrap();
+    assert_eq!(l_dt4.to_string(), "1979-05-27T07:32:00.5");
+
+    let str5 = "1979-05-27 07:32:00.5";
+    let l_dt5 = LocalDateTime::try_from(str5).unwrap();
+    assert_eq!(l_dt5.to_string(), "1979-05-27T07:32:00.5");
+}
--- src/value/local_time.rs
+++ src/value/local_time.rs
@@ -27,8 +27,56 @@ impl std::fmt::Display for LocalTime {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "{:02}:{:02}:{:02}.{:03}",
+            "{:02}:{:02}:{:02}.{}",
             self.hour, self.minute, self.second, self.subseconds
         )
     }
+}
+
+impl TryFrom<&str> for LocalTime {
+    type Error = ();
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let mut it: [&str; 4] = ["0", "0", "0", "0"];
+        if !value.contains(':') || value.len() < 5 {
+            return Err(());
+        }
+        for (i, v) in value.split(':').enumerate() {
+            if v.contains('.') {
+                let (left, right) = v.split_once('.').unwrap();
+                it[3] = right;
+                it[i] = left;
+            } else {
+                it[i] = v;
+            }
+        }
+        let hour = it[0].parse().map_err(|_| ())?;
+        let minute = it[1].parse().map_err(|_| ())?;
+        let second = it[2].parse().map_err(|_| ())?;
+        let subseconds = it[3].parse().map_err(|_| ())?;
+        Ok(Self::new(hour, minute, second, subseconds))
+    }
+}
+
+impl TryFrom<String> for LocalTime {
+    type Error = ();
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        value.as_str().try_into()
+    }
+}
+
+#[test]
+fn parse_rfc3339() {
+    let str0 = "02:10:00.987";
+    let l_time0 = LocalTime::try_from(str0).unwrap();
+    assert_eq!(l_time0.to_string(), str0);
+
+    let str1 = "07:32:00";
+    let l_time1 = LocalTime::try_from(str1).unwrap();
+    assert_eq!(l_time1.to_string(), "07:32:00.0");
+
+    let str2 = "07:48";
+    let l_time2 = LocalTime::try_from(str2).unwrap();
+    assert_eq!(l_time2.to_string(), "07:48:00.0");
 }
